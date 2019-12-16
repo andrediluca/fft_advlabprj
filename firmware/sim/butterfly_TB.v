8ea093284196f254888f66120725fc55619c1b77
Giuseppe Gebbia <giuseppegebbia92@gmail.com>
2019-12-16 14:29:43 +0100

    implement overflow/underflow protection by clipping to max/min

--- firmware/sim/butterfly_TB.v
+++ firmware/sim/butterfly_TB.v
@@ -3,6 +3,8 @@
 module butterfly_TB(
 );
 
+parameter tclk = 20;
+
 reg clk = 0;
 reg enable = 0;
 reg signed [15:0] xa_re;
@@ -16,6 +18,7 @@ wire signed [15:0] Xa_im;
 wire signed [15:0] Xb_re;
 wire signed [15:0] Xb_im;
 
+// unit under test
 butterfly uut(
 	.clk(clk),
 	.enable(enable),
@@ -32,24 +35,27 @@ butterfly uut(
 );
 
 
-always #10 clk = ~clk;
+always #(tclk/2) clk = ~clk;
 
 always begin
 
+	// TODO: more comprehensive testing
 	#100;
 	enable <= 1'b1;
-	xa_re <= 1;
-	xa_im <= 0;
+	xa_re <= 2;
+	xa_im <= 1;
 	xb_re <= 3;
 	xb_im <= 0;
 	W_re  <= 2;
 	W_im  <= 0;
+	#(tclk)
+	xa_re <= 2**15-1;
+	xa_im <= -(2**15-1);
+	xb_re <= 3;
+	xb_im <= 10;
+	W_re  <= 2;
+	W_im  <= 0;
 
 end
-
-
-
-
-
 
 endmodule
--- firmware/src/butterfly.v
+++ firmware/src/butterfly.v
@@ -1,43 +1,80 @@
-
-/******************************************
-* Butterfly computation   
-* the output is registered and so delayed 
+/********************************************
+* Butterfly computation
+* the output is registered and so is delayed
 * of 1-clk cycle
-*******************************************/
+********************************************/
 
 module butterfly(
-  // control signals
-  input wire clk,
-  input wire enable,
-  // input data
-  input wire signed [15:0] xa_re, // real part of a
-  input wire signed [15:0] xa_im, // im part of b
-  input wire signed [15:0] xb_re,
-  input wire signed [15:0] xb_im,
-  input wire signed [15:0] W_re,  // real part of twiddle from ROM
-  input wire signed [15:0] W_im,  // im part of twiddle from ROM
-  // output data
-  output reg signed [15:0] Xa_re,
-  output reg signed [15:0] Xa_im,
-  output reg signed [15:0] Xb_re,
-  output reg signed [15:0] Xb_im
+	// control signals
+	input wire clk,
+	input wire enable,
+	// input data
+	input wire signed [15:0] xa_re, // real part of a
+	input wire signed [15:0] xa_im, // im part of b
+	input wire signed [15:0] xb_re,
+	input wire signed [15:0] xb_im,
+	input wire signed [15:0] W_re,  // real part of twiddle from ROM
+	input wire signed [15:0] W_im,  // im part of twiddle from ROM
+	// output data
+	output reg signed [15:0] Xa_re,
+	output reg signed [15:0] Xa_im,
+	output reg signed [15:0] Xb_re,
+	output reg signed [15:0] Xb_im
 );
 
-reg  signed [15:0] diff_re;
-reg  signed [15:0] diff_im;
+
+function signed [15:0] adder;
+	/*******************************
+	* Function to set fixed value for
+	* overflow or underflow case
+	*********************************/
+	input reg signed [15:0] a;
+	input reg signed [15:0] b;
+	input op;
+	reg signed [16:0] res;
+
+	begin
+		if (op == 1'b0) begin
+			res = a+b;
+		end else begin
+			res = a-b;
+		end
+
+		if (res[16:15] == 2'b01) begin
+			res = 2**15-1;
+		end else if (res[16:15] == 2'b10) begin
+			res = -(2**15-1);
+		end
+
+		adder = res[15:0];
+
+	end
+endfunction
+
+reg signed [15:0] diff_re;
+reg signed [15:0] diff_im;
+reg signed [15:0] mult_1, mult_2, mult_3, mult_4;
 
 always @(posedge clk) begin
-  if (enable) begin
-    Xa_re <= xa_re + xb_re;
-    Xa_im <= xa_im + xb_im;
-    
-    diff_re = xa_re - xb_re;
-    diff_im = xa_im - xb_im;
-    Xb_re <= diff_re * W_re - diff_im * W_im; 
-    Xb_im <= diff_re * W_im + diff_im * W_re;
-  end
+	if (enable) begin
+
+		Xa_re <= adder(xa_re, xb_re, 1'b0);
+		Xa_im <= adder(xa_im, xb_im, 1'b0);
+
+		diff_re = adder(xa_re, xb_re, 1'b1);
+		diff_im = adder(xa_im, xb_im, 1'b1);
+
+		mult_1 = diff_re * W_re;
+		mult_2 = diff_im * W_im;
+		Xb_re <= adder(mult_1, mult_2, 1'b1);
+
+		mult_3 = diff_re * W_im;
+		mult_4 = diff_im * W_re;
+		Xb_im <= adder(mult_3, mult_4, 1'b0);
+	end
+
 end
-    
-    
- 
+
 endmodule
+
+
