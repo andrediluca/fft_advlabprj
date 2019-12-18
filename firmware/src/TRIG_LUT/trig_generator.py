9e4c850558d8e70a7a96c3cefd574c42283438d3
diluca <andrea.diluca@unitn.it>
2019-12-18 18:24:48 +0100

    script to produce sin and cos tables (hex format)

---
+++ firmware/src/TRIG_LUT/trig_generator.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+#Conversion to hex
+#Ref: https://stackoverflow.com/questions/7822956/how-to-convert-negative-integer-value-to-hex-in-python
+def tohex(val, nbits):
+    return hex((val + (1 << nbits)) % (1 << nbits))
+
+f = open("cos.list", "w")
+
+for i in range(512):
+   # f.write("%.9f\n" %int(np.cos(2*np.pi/1024*i)*10**4))
+     f.write(tohex(int(np.cos(2*np.pi/1024*i)*10**4),16)[2:]+"\n")
+f.close()
+
+f = open("sin.list", "w")
+
+for i in range(512):
+   # f.write("%.9f\n" %int(np.cos(2*np.pi/1024*i)*10**4))
+     f.write(tohex(int(np.sin(2*np.pi/1024*i)*10**4),16)[2:]+"\n")
+f.close()
