d8b9a8fa623d1bac5a0103d2c7d70566779ea1c9
diluca <andrea.diluca@unitn.it>
2020-01-23 14:58:28 +0100

    Script modified to generate only lut.

--- software/lut_generator.py
+++ software/lut_generator.py
@@ -5,22 +5,22 @@ import numpy as np
 def tohex(val, nbits):
     return hex((val + (1 << nbits)) % (1 << nbits))
 
-frequency = [1000, 10000]
+frequency = 1
 npoints = 1024
 nbits = 8
-for freq in frequency:
-    cos_name = "cos_%d.mem" %freq
-    sin_name = "sin_%d.mem"%freq
 
-    f = open(cos_name, "w")
-    for i in range(npoints):
-       # f.write("%.9f\n" %int(np.cos(2*np.pi/1024*i)*10**4))
-         f.write(tohex(int(np.cos(2*np.pi/npoints*i * freq)*10**4),nbits)[2:]+"\n")
-    f.close()
+cos_name = "cos.mem"
+sin_name = "sin.mem"
 
-    f = open(sin_name, "w")
+f = open(cos_name, "w")
+for i in range(npoints):
+   # f.write("%.9f\n" %int(np.cos(2*np.pi/1024*i)*10**4))
+     f.write(tohex(int(np.cos(2*np.pi/npoints*i * freq)*10**4),nbits)[2:]+"\n")
+f.close()
 
-    for i in range(npoints):
-       # f.write("%.9f\n" %int(np.cos(2*np.pi/1024*i)*10**4))
-         f.write(tohex(int(-np.sin(2*np.pi/npoints*i * freq)*10**4),nbits)[2:]+"\n")
-    f.close()
+f = open(sin_name, "w")
+
+for i in range(npoints):
+   # f.write("%.9f\n" %int(np.cos(2*np.pi/1024*i)*10**4))
+     f.write(tohex(int(-np.sin(2*np.pi/npoints*i * freq)*10**4),nbits)[2:]+"\n")
+f.close()
