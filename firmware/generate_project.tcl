691709c292316bf6af44bcadd32806f9e88e525d
Giuseppe Gebbia <giuseppegebbia92@gmail.com>
2020-02-03 18:12:59 +0100

    fix xadc conf

--- firmware/generate_project.tcl
+++ firmware/generate_project.tcl
@@ -13,3 +13,18 @@ add_files -fileset constrs_1 "${root_dir}/xdc/"
 add_files -fileset sim_1 "${root_dir}/sim/"
 set_property file_type Verilog [get_files -of [get_filesets [list sources_1 sim_1]]]
 update_compile_order -fileset sources_1
+
+
+create_ip -name xadc_wiz -vendor xilinx.com -library ip -version 3.3 -module_name xadc_wiz_0
+set_property -dict [list \
+	CONFIG.INTERFACE_SELECTION {None} \
+	CONFIG.ENABLE_AXI4STREAM {true} \
+	CONFIG.ADC_CONVERSION_RATE {48} \
+	CONFIG.ENABLE_RESET {true} \
+	CONFIG.OT_ALARM {false} \
+	CONFIG.USER_TEMP_ALARM {false} \
+	CONFIG.VCCINT_ALARM {false} \
+	CONFIG.VCCAUX_ALARM {false} \
+	CONFIG.SINGLE_CHANNEL_SELECTION {VAUXP0_VAUXN0} \
+	CONFIG.BIPOLAR_OPERATION {true}] \
+	[get_ips xadc_wiz_0]
